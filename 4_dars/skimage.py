d1d7da4846d39fe9de77b41c72bd24de95c24d2e
NurAI <mansurjanov01@gmail.com>
2025-03-08 05:41:49 +0900

    Add files via upload

---
+++ 4_dars/skimage.py
@@ -0,0 +1,23 @@
+#CV librarie
+# import cv2 
+# import numpy as np
+# import matplotlib.pyplot as plt
+# image_path = "cat_0062.jpg"
+# image = cv2.imread(image_path)
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
+# cv2.destroyWindow()
+
+# PIL librarie
+# from PIL import Image
+# image_path = "cat_0062.jpg"
+# image = Image.open(image_path)
+# image.show() 
+
+# SKimage librarie
+# from skimage import io 
+# import matplotlib.pyplot as plt
+# image_path = "cat_0062.jpg" 
+# image = io.imread(image_path)
+# plt.imshow(image)
+# plt.show()
